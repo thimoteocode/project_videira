7891b9861f9f17aea66d728c2915a6cf4afb3e78
thimoteo <thimoteo.oliveira@nre.tec.br>
2022-04-07 18:22:23 -0300

    delete, edit, search

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Pessoa
 
 class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type": "date"}
+        )    
+    )
+
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'data_nascimento', 'ativo']
